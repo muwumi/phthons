c0866bdb03f78e36e7c0c508c02e9d8ed4be1a4e
LSH <tkdgjs9528@naver.com>
2024-02-01 15:08:52 +0900

    오후1차

--- teamProject/procData.py
+++ teamProject/procData.py
@@ -1,3 +1,4 @@
+import math
 import pandas, os, sys, seaborn, matplotlib, csv, numpy
 import matplotlib.pyplot as plt
 
@@ -8,23 +9,29 @@ csvDataFrame = pandas.read_csv(csvPath, sep=',', encoding='utf-8-sig')
 print(csvDataFrame)
 print(type(csvDataFrame))
     #데이터 컨트롤
+bTitle = csvDataFrame.loc[0 : , ['제목']]
 price = csvDataFrame.loc[0 : , ['가격']]
 ePrice = csvDataFrame.loc[0 : , ['e북가격']]
 rank = csvDataFrame.loc[0 : , ['등수']]
+bTitleList2D = bTitle.values.tolist()
 priceList2D = price.values.tolist()
 ePriceList2D = ePrice.values.tolist()
 rankList2D = rank.values.tolist()
 
+bTitleList = []
 priceList = []
 ePriceList = []
 rankList = []
 for i in range(len(priceList2D)):
     #e북 가격이 없는 경우 별도로 처리를 해줘야함
     if(ePriceList2D[i][0]!=' '):
+        bTitleList.append(bTitleList2D[i][0])
         priceList.append(int(priceList2D[i][0].replace(',', '')))
         ePriceList.append(int(ePriceList2D[i][0].replace(',', '')))
         rankList.append(int(rankList2D[i][0].split(' ')[1].replace('위', '')))
-    
+
+print('='*50, 'bTitleList')
+print(bTitleList)    
 print('='*50, 'priceList')
 print(priceList)
 print('='*50, 'ePriceList')
@@ -32,6 +39,7 @@ print(ePriceList)
 print('='*50, 'rankList')
 print(rankList)
 
+'''
 #그래프 그려보기
     #plt.plot([x값인자], [y값인자])
     #데이터 정렬(가격)
@@ -43,49 +51,48 @@ z = ePriceList
 rate = []
 for i in range(len(priceList)):
     print(i)
-    rate.append(int(z[i])/int(x[i])) 
-print('=======>rate : ' ,rate, len(rate))
+    rate.append(round(int(ePriceList[i])/int(priceList[i]), 3)) 
+print('=======>rate : {}, {}개'.format(rate, len(rate)))
 
 sortedPriceList = sorted(priceList)
 maxX = sortedPriceList[len(priceList)-1]
 minX = sortedPriceList[0]
-print(priceList)
+print('=======>priceList : {}, {}'.format(priceList, len(priceList)))
+print('=======>epriceList : {}, {}'.format(ePriceList, len(ePriceList)))
 
-'''
-    # 산포도 그리기
-plt.scatter(x, y, label='Unsorted Data', color='blue', marker='o')
 
-    # 그래프에 제목과 축 레이블 추가
-plt.title('scatter of price and rank')
-plt.xlabel('price')
-plt.ylabel('rank')
-'''
-'''
-    # 산포도 그리기
-plt.scatter(x, z, label='Unsorted Data', color='blue', marker='o')
+# 데이터 생성
+categories = []
+for i in range(len(priceList)):
+    strInput = '[{}] {}'.format(i, priceList[i])
+    categories.append(strInput)
+print(categories)
+plt.scatter(categories, rate, s=5)
 
-    # 그래프에 제목과 축 레이블 추가
-plt.title('scatter of price and ebookPrice')
-plt.xlabel('price')
-plt.ylabel('ePrice')
-'''
-'''
-    #히스토그램그리기(빈도)
-plt.hist(x, label='paper-price',color='blue')
 
-plt.hist(z, label='ebook-price', color='yellow')
-'''
+# 그래프에 제목과 축 레이블 추가
+plt.title('Sum of Paper Price, Ebook Price, and Rank')
+plt.xlabel('Categories')
+plt.ylabel('Values')
 
-plt.scatter(x, rate)
-plt.title('ebookPrice/paperPrice')
-plt.xlabel('paper book price')
-plt.ylabel('rate(ebook/paper)')
-plt.xlim(minX-100, maxX+100)
-plt.ylim(0, 1)
+# 그래프 표시
+plt.show()'''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+# DataFrame의 plot() 메서드를 사용하여 선 그래프 그리기
+csvDataFrame.plot(x='제목', y='가격', kind='line', marker='o', linestyle='-', color='blue', label='Line Plot')
+
+# 그래프에 제목과 축 레이블 추가
+plt.title('Line Plot using pandas')
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
 
 # 범례 추가
-plt.legend('이렇게 해볼까')
+plt.legend()
 
 # 그래프 표시
 plt.show()
-
--- teamProject/webCrawling.py
+++ teamProject/webCrawling.py
@@ -130,9 +130,9 @@ for i in range(1, endPage+1):
         dataList1D = []#2D그릇에 넣을 1D 데이터, 매번 초기화해야 하기 때문에 for문 안에 넣기
         if(len(dataResult2D)<dataNum): #'='을 넣으면 마지막에 하나더 삽입 됨
             dataList1D.append(title[0].text)
-            dataList1D.append(price[0].text)
+            dataList1D.append(price[0].text.replace(',', ''))
             if(len(ePrice)==2):
-                dataList1D.append(ePrice[1].text)
+                dataList1D.append(ePrice[1].text.replace(',', ''))
             else :
                 dataList1D.append(' ') 
             dataList1D.append(date[0].text)
@@ -257,10 +257,47 @@ rankList = []
 for i in range(len(priceList2D)):
     #e북 가격이 없는 경우 별도로 처리를 해줘야함
     if(ePriceList2D[i][0]!=' '):
+        priceList.append(int(priceList2D[i][0]))
+        ePriceList.append(int(ePriceList2D[i][0]))
+        rankList.append(int(rankList2D[i][0].split(' ')[1].replace('위', '')))
+    
+print('='*50, 'priceList')
+print(priceList)
+print('='*50, 'ePriceList')
+print(ePriceList)
+print('='*50, 'rankList')
+print(rankList)
+
+#csv 읽어오고 데이터 가져오기
+    #csv 읽기
+csvPath = r'D:\LSH\workspace\phthons\teamProject{}'.format(fName)
+csvDataFrame = pandas.read_csv(csvPath, sep=',', encoding='utf-8-sig')
+print(csvDataFrame)
+print(type(csvDataFrame))
+    #데이터 컨트롤
+bTitle = csvDataFrame.loc[0 : , ['제목']]
+price = csvDataFrame.loc[0 : , ['가격']]
+ePrice = csvDataFrame.loc[0 : , ['e북가격']]
+rank = csvDataFrame.loc[0 : , ['등수']]
+bTitleList2D = bTitle.values.tolist()
+priceList2D = price.values.tolist()
+ePriceList2D = ePrice.values.tolist()
+rankList2D = rank.values.tolist()
+
+bTitleList = []
+priceList = []
+ePriceList = []
+rankList = []
+for i in range(len(priceList2D)):
+    #e북 가격이 없는 경우 별도로 처리를 해줘야함
+    if(ePriceList2D[i][0]!=' '):
+        bTitleList.append(bTitleList2D[i][0])
         priceList.append(int(priceList2D[i][0].replace(',', '')))
         ePriceList.append(int(ePriceList2D[i][0].replace(',', '')))
         rankList.append(int(rankList2D[i][0].split(' ')[1].replace('위', '')))
-    
+
+print('='*50, 'bTitleList')
+print(bTitleList)    
 print('='*50, 'priceList')
 print(priceList)
 print('='*50, 'ePriceList')
@@ -279,48 +316,29 @@ z = ePriceList
 rate = []
 for i in range(len(priceList)):
     print(i)
-    rate.append(int(z[i])/int(x[i])) 
-print('=======>rate : ' ,rate, len(rate))
+    rate.append(round(int(ePriceList[i])/int(priceList[i]), 3)) 
+print('=======>rate : {}, {}개'.format(rate, len(rate)))
 
 sortedPriceList = sorted(priceList)
 maxX = sortedPriceList[len(priceList)-1]
 minX = sortedPriceList[0]
-print(priceList)
+print('=======>priceList : {}, {}'.format(priceList, len(priceList)))
+print('=======>epriceList : {}, {}'.format(ePriceList, len(ePriceList)))
 
-'''
-    # 산포도 그리기
-plt.scatter(x, y, label='Unsorted Data', color='blue', marker='o')
 
-    # 그래프에 제목과 축 레이블 추가
-plt.title('scatter of price and rank')
-plt.xlabel('price')
-plt.ylabel('rank')
-'''
-'''
-    # 산포도 그리기
-plt.scatter(x, z, label='Unsorted Data', color='blue', marker='o')
+# 데이터 생성
+categories = []
+for i in range(len(priceList)):
+    strInput = '[{}] {}'.format(i, priceList[i])
+    categories.append(strInput)
+print(categories)
+plt.scatter(categories, rate, s=5)
 
-    # 그래프에 제목과 축 레이블 추가
-plt.title('scatter of price and ebookPrice')
-plt.xlabel('price')
-plt.ylabel('ePrice')
-'''
-'''
-    #히스토그램그리기(빈도)
-plt.hist(x, label='paper-price',color='blue')
 
-plt.hist(z, label='ebook-price', color='yellow')
-'''
-
-plt.scatter(x, rate)
-plt.title('ebookPrice/paperPrice')
-plt.xlabel('paper book price')
-plt.ylabel('rate(ebook/paper)')
-plt.xlim(minX-100, maxX+100)
-plt.ylim(0, 1)
-
-# 범례 추가
-plt.legend()
+# 그래프에 제목과 축 레이블 추가
+plt.title('Sum of Paper Price, Ebook Price, and Rank')
+plt.xlabel('Categories')
+plt.ylabel('Values')
 
 # 그래프 표시
 plt.show()
